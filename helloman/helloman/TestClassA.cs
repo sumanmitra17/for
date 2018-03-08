51ab78933939ae82ea07bb42ba824e8d4a6f2ec1
sumanmitra17 <suman.mitra@indusnet.co.in>
2018-03-08 18:18:54 +0530

    first commit

---
+++ helloman/helloman/TestClassA.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections;
+
+namespace helloman
+{
+    [TestFixture]
+    public class TestClassA
+    {
+        public static IWebDriver dr;
+
+        [Test]
+        public void setup()
+        {
+            // dr = new ChromeDriver();
+
+
+            IWebDriver dr = new ChromeDriver(@"E:\Suman\Softwares\Cell\Visual Studio\Driver");
+            dr.Navigate().GoToUrl("http://172.17.3.13");
+            dr.Manage().Window.Maximize();
+
+            //  dr.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+
+           // TestClassSuite.TestSuite(dr);       //  Call the TestSuite Class by calling object and passing public variables
+
+        }
+
+
+        /*   [OneTimeTearDown]
+           public void EndReport()
+           {
+               dr.Close();                        //   Closing the Browser
+           }*/
+    }
+}
